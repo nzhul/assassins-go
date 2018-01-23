f5333a4c8cd2c696694a9dee3e15a8db15ad61fa
nzhul <dobromirivanov1@gmail.com>
2018-01-23 08:16:13 +0200

    update

---
+++ Assassins Go/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class EndScreen : MonoBehaviour
+{
+
+    public PostProcessingProfile blurProfile;
+    public PostProcessingProfile normalProfile;
+    public PostProcessingBehaviour cameraPostProcess;
+
+    public void EnableCameraBlur(bool state)
+    {
+        if (cameraPostProcess != null && blurProfile != null && normalProfile != null)
+        {
+            cameraPostProcess.profile = (state) ? blurProfile : normalProfile;
+        }
+    }
+
+}
