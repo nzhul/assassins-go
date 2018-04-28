798d7380fdeff4fccfe2389ef51d3feefd92dabd
Dobromir Ivanov <dobromirivanov1@gmail.com>
2018-04-28 12:21:34 +0300

    update

---
+++ Assassins Go/Assets/Scripts/LevelLabel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class LevelLabel : MonoBehaviour
+{
+    Text _text;
+
+    void Awake()
+    {
+		_text = GetComponent<Text>();
+
+		if (_text != null)
+		{
+			_text.text = SceneManager.GetActiveScene().name;
+		}
+    }
+}
